98c3efbdaeca299cb4081c50bab484378eaaff45
manojkulkarni <mnjpkulkarni@gmail.com>
2017-08-20 20:35:26 -0400

    Initial Commit

---
+++ My Social Network/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My Social Network
+//
+//  Created by Manoj Kulkarni on 8/20/17.
+//  Copyright © 2017 Manoj Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
